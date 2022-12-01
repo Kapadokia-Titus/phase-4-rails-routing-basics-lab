9efb22be9663e15bc6c7709fed151f6cee42e86e
kapadokia <ktnyandoch@gmail.com>
2022-12-01 03:58:19 +0300

    rails routes configures

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+    def index
+        all_students = Student.all 
+        render json: all_students
+    end
+
+    def grades
+        grades = Student.order("grade DESC")
+        render json: grades
+    end
+end
